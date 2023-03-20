dc16f7ba28f5345e90ee68345bc3df154cf2ffa4
Bart Ramaekers <bartramaekers99@gmail.com>
2023-03-20 23:23:48 +0100

    WIP: implement CFastArray

--- proud/fastarray.v
+++ proud/fastarray.v
@@ -17,6 +17,12 @@ mut:
 	suspend_shrink bool
 }
 
+pub fn new_fast_array_from_ptr[T](data &T, int count) CFastArray[T] {
+	mut array := CFastArray[T]{}
+
+	array.insert_range(0, data, count)
+}
+
 pub fn (mut c CFastArray[T]) get_grow_policy() GrowPolicy {
 	return c.grow_policy
 }
@@ -35,6 +41,48 @@ pub fn (mut c CFastArray[T]) get_min_capacity() int {
 
 pub fn (mut c CFastArray[T]) suspend_shrink() {
 	c.suspend_shrink = true
+}
+
+pub fn (mut a CFastArray[T]) get_recommended_capacity(wanted_length int) int {
+	grow_by := match a.grow_policy {
+		.normal {
+			mut tmp := a.data.len / 8
+			tmp = math.min(tmp, 1024)
+			math.max(tmp, 4)
+		}
+		.high_speed {
+			mut tmp := a.data.len / 8
+			tmp = math.max(tmp, 16)
+			tmp = math.max(tmp, 64 / a.data.element_size)
+			math.min(tmp, 1024)
+		}
+		.low_memory {
+			math.max(a.min_capacity, wanted_length)
+		}
+	}
+
+	v1 := a.data.cap
+	v2 := wanted_length + grow_by
+	mut diff := 0
+	if v1 > v2 {
+		diff = v1 - v2
+	} else {
+		diff = v2 - v1
+	}
+
+	if !a.suspend_shrink {
+		if diff > grow_by {
+			return math.max(a.min_capacity, v2)
+		} else {
+			return math.max(a.min_capacity, v1)
+		}
+	}
+
+	if diff > grow_by {
+		return math.max(a.data.cap, v2)
+	}
+
+	return math.max(a.data.cap, v1)
 }
 
 pub fn (mut a CFastArray[T]) set_capacity(cap int) {
@@ -115,8 +163,8 @@ pub fn (mut a CFastArray[T]) set_count(count int) {
 	if count > a.data.len {
 		a.add_count(count - a.data.len)
 	} else if count < a.data.len {
-		// todo: get_recommended_capacity(count)
-		a.set_capacity(count)
+		capacity := a.get_recommended_capacity(count)
+		a.set_capacity(capacity)
 	}
 }
 
@@ -124,8 +172,8 @@ pub fn (mut a CFastArray[T]) add_count(count int) {
 	assert count >= 0
 
 	if a.data.len + count > a.data.cap {
-		// todo: get_recommended_capacity(a.data.len + count)
-		a.set_capacity(a.data.len + count)
+		capacity := a.get_recommended_capacity(a.data.len + count)
+		a.set_capacity(capacity)
 	}
 
 	a.data << []u8{len: count}
@@ -150,8 +198,107 @@ pub fn (mut a CFastArray[T]) add_range(data voidptr, count int) {
 	}
 }
 
+pub fn (mut a CFastArray[T]) resize(count int) {
+	a.set_count(count)
+}
+
 pub fn (mut a CFastArray[T]) bound_check(index int) ! {
 	if index < 0 || index >= a.data.len {
 		return error('Out of bounds')
+	}
+}
+
+pub fn (mut a CFastArray[T]) get_capacity() int {
+	return a.data.cap
+}
+
+pub fn (mut a CFastArray[T]) get_count() int {
+	return a.data.len
+}
+
+pub fn (mut a CFastArray[T]) size() int {
+	return a.data.len
+}
+
+pub fn (mut a CFastArray[T]) is_empty() bool {
+	return a.data.len == 0
+}
+
+pub fn (mut a CFastArray[T]) copy_range_to(mut dst &CFastArray[T], src_offset int, count int) {
+	assert src_offset + count > a.data.len || src_offset >= 0 || count >= 0
+
+	dst.set_count(count)
+
+	mut our_ptr := &u8(a.get_data().data)
+	unsafe {
+		our_ptr += src_offset
+		vmemcpy(dst.get_data().data, our_ptr, a.data.element_size * count)
+		dst.get_data().data
+	}
+}
+
+pub fn (mut a CFastArray[T]) copy_to(mut dst &CFastArray[T]) {
+	a.copy_range_to(mut dst, 0, a.data.len)
+}
+
+pub fn (mut a CFastArray[T]) copy_from(mut src &CFastArray[T]) {
+	src.copy_to(mut a)
+}
+
+pub fn (mut a CFastArray[T]) remove_range(index int, count int) {
+	assert index >= 0
+	assert count >= 0
+
+	real_count := math.min(count, a.data.len - index)
+	amount := a.data.len - index + real_count
+
+	mut dest_ptr := &u8(a.data.data)
+	mut src_ptr := &u8(a.data.data)
+	unsafe {
+		dest_ptr += index
+		src_ptr += index + real_count
+
+		vmemmove(dest_ptr, src_ptr, a.data.element_size * amount)
+	}
+
+	a.set_count(a.data.len - real_count)
+}
+
+pub fn (mut a CFastArray[T]) remove_at(index int) {
+	a.remove_range(index, 1)
+}
+
+pub fn (mut a CFastArray[T]) remove_one_by_value(value T) bool {
+	for i := 0; i < a.data.len; i++ {
+		if a.data[i] == value {
+			a.remove_at(i)
+			return true
+		}
+	}
+
+	return false
+}
+
+pub fn (mut a CFastArray[T]) find_by_value(value T) int {
+	for i := 0; i < a.data.len; i++ {
+		if a.data[i] == value {
+			return i
+		}
+	}
+
+	return -1
+}
+
+pub fn (mut a CFastArray[T]) contains(value T) bool {
+	return a.find_by_value(value) >= 0
+}
+
+pub fn (mut a CFastArray[T]) equals(mut rhs &CFastArray[T]) bool {
+	if a.data.len != rhs.data.len {
+		return false
+	}
+
+	unsafe {
+		return vmemcmp(a.data.len, rhs.get_data().data, a.data.len * a.data.element_size) == 0
 	}
 }
--- proud/hostidarray.v
+++ proud/hostidarray.v
@@ -1,5 +1,6 @@
 module proud
 
+// todo: can't use enum as generic even if marked as u8..
 struct HostIDArray {
-
+	CFastArray[u8]
 }
